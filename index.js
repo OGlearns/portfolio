658fe0f9764017449ffbca9981da602eca97db7e
Samuel Ogundare <s.ogundare@gmail.com>
2023-03-30 16:02:18 -0400

    added js file index. add easy scroll

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+    // Get links to attach easy scroll
+    const links = document.querySelectorAll('.link')
+    links.forEach(element => {
+        element.addEventListener('click', easyScroll)
+    })
+    
+    
+})
+
+
+
+
+
+function easyScroll(event) {
+event.preventDefault()
+const targetId = this.getAttribute('href')
+const targetPosition = document.querySelector(targetId).offsetTop
+window.scrollTo({
+    top: targetPosition,
+    behavior: 'smooth'
+})
+}
